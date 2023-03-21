59451eff0d8fa9dc408b5b9135b489ab3936dacf
Stepan Koryagin <stepanswim@gmail.com>
2023-03-21 12:07:09 +0200

    Initial commit, add connection with PostgresSQL

---
+++ src/main/java/com/stepan/eshop/spring_eshop/SpringEshopApplication.java
@@ -0,0 +1,13 @@
+package com.stepan.eshop.spring_eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stepan/eshop/spring_eshop/SpringEshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepan.eshop.spring_eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
